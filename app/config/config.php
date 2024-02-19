a449528c89662734ec728ada26967ddc346109c0
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-02-19 16:00:01 +0530

    Define common file paths in `/app/config/config.php`. Constants are not yet used

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// /app/ folder path
+define('APP_ROOT', dirname(__DIR__));
+
+// site name
+define('SITE_NAME', 'MVC');
+
+// URL root
+define('URL_ROOT', 'http://localhost/' . strtolower(SITE_NAME) . '/');
