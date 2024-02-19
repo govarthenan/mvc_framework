6830b3c60292a66e25822469e3eea4fb6ac944f4
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-02-19 16:00:01 +0530

    Define common file paths in `/app/config/config.php`. Constants are not yet used

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,10 @@
  * Require core files and other configs.
  */
 
+// load config
+require_once '../app/config/config.php';
+
+// load libraries
 require_once '../app/libraries/Core.php';
 require_once '../app/libraries/Controller.php';
 require_once '../app/libraries/Database.php';
