9ecbdb74f565fc9c1f8fb66cae258815db9a9481
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-02-20 22:43:00 +0530

    Remove require statements as autoload supersedes them

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,7 +7,7 @@
 // load config
 require_once '../app/config/config.php';
 
-// load libraries
-require_once APP_ROOT . '/libraries/Core.php';
-require_once APP_ROOT . '/libraries/Controller.php';
-require_once APP_ROOT . '/libraries/Database.php';
+// load libraries - now handled by autoload
+// require_once APP_ROOT . '/libraries/Core.php';
+// require_once APP_ROOT . '/libraries/Controller.php';
+// require_once APP_ROOT . '/libraries/Database.php';
