28000de2839d8b7b4682489f3265dab231b3f405
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-02-24 15:52:17 +0530

    Add functionality to log autload activities to JS console

--- app/config/MyAutoLoader.php
+++ app/config/MyAutoLoader.php
@@ -6,11 +6,30 @@
  * @method void loadLibraries() Load classes found in `app/libraries/` e.g: Core, Controller, Model
  * @method void loadControllers() Load classses found in `app/controllers/` e.g: Pages, Posts
  * @method void loadModels() Load models found in `app/models`
+ * @method void consoleLog() Log to JS console regarding autoload activities.
  *
  * @param string $className The name of the class to be imported
  */
 class MyAutoLoader
 {
+
+    /**
+     * Log to JS console about each autload method that is run.
+     *
+     * @param boolean $loadStatus Indicates whether the method loaded a class or not.
+     * @param string $classPath The file path of the class that was passed in.
+     * @param string $loaderMethod The specific method the log entry is about.
+     */
+    public static function consoleLog(int $loadStatus, string $classPath, string $loaderMethod)
+    {
+        if ($loadStatus == true) {
+            $console_message = 'load - ' . $classPath . ' - ' . $loaderMethod;
+        } else {
+            $console_message = 'NOT load - ' . $classPath . ' - ' . $loaderMethod;
+        }
+        echo "<script type=\"text/javascript\">console.log(\"$console_message\")</script>\n";
+    }
+
     public static function loadLibraries($className)
     {
         // define directory to search
@@ -20,9 +39,9 @@ class MyAutoLoader
         // if file exists, load file and log
         if (file_exists($class_path)) {
             require_once $class_path;
-            $autoload_log[] = 'load - ' . $class_path;
+            self::consoleLog(true, $class_path, __FUNCTION__);
         } else {
-            $autoload_log[] = 'NOT load - ' . $class_path;
+            self::consoleLog(false, $class_path, __FUNCTION__);
         }
     }
 
@@ -35,9 +54,9 @@ class MyAutoLoader
         // if file exists, load file and log
         if (file_exists($class_path)) {
             require_once $class_path;
-            $autoload_log[] = 'load - ' . $class_path;
+            self::consoleLog(true, $class_path, __FUNCTION__);
         } else {
-            $autoload_log[] = 'NOT load - ' . $class_path;
+            self::consoleLog(false, $class_path, __FUNCTION__);
         }
     }
 
@@ -50,9 +69,9 @@ class MyAutoLoader
         // if file exists, load file and log
         if (file_exists($class_path)) {
             require_once $class_path;
-            $autoload_log[] = 'load - ' . $class_path;
+            self::consoleLog(true, $class_path, __FUNCTION__);
         } else {
-            $autoload_log[] = 'NOT load - ' . $class_path;
+            self::consoleLog(false, $class_path, __FUNCTION__);
         }
     }
 }
