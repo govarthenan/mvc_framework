a0a7f29de19559cecf26850e6a020aeaa38799e4
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-02-24 18:34:34 +0530

    Move importing of `MyAutoLoader` class from `config.php` to `bootstrap.php`

--- app/config/config.php
+++ app/config/config.php
@@ -10,7 +10,6 @@ define('SITE_NAME', 'MVC');
 define('URL_ROOT', 'http://localhost/' . strtolower(SITE_NAME) . '/');
 
 // import MyAutoLoader class and register static methods for autloading
-require_once APP_ROOT . '/config/MyAutoLoader.php';  // class containing all autoload functions
 spl_autoload_register('MyAutoLoader::autoLoadLibraries');  // register static function's name as string
 spl_autoload_register('MyAutoLoader::autoLoadControllers');
 spl_autoload_register('MyAutoLoader::autoLoadModels');
